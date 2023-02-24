809781a7fc334fd7b314de22865f32d580e19019
Cory Platt <corplatt@publicisgroupe.net>
2023-02-24 14:40:12 -0800

    fix to script

--- bq_test_create.py
+++ bq_test_create.py
@@ -17,15 +17,15 @@ with models.DAG(
         start_date=datetime.datetime(2023, 1, 1),
         default_args=default_dag_args) as dag:
 
-table_creation = BigQueryExecuteQueryOperator(
-    task_id="make a table in bigquery",
-    sql="""
-    SELECT 0 AS Test_1, 1 AS Test_2
-    """,
-    destination_dataset_table=f"digitas-sephora.Test_Data.airflow_example",
-    write_disposition="WRITE_TRUNCATE",
-    gcp_conn_id="sephora_bigquery_connection",
-    use_legacy_sql=False,
-)
+    table_creation = BigQueryExecuteQueryOperator(
+        task_id="make a table in bigquery",
+        sql="""
+        SELECT 0 AS Test_1, 1 AS Test_2
+        """,
+        destination_dataset_table=f"digitas-sephora.Test_Data.airflow_example",
+        write_disposition="WRITE_TRUNCATE",
+        gcp_conn_id="sephora_bigquery_connection",
+        use_legacy_sql=False,
+    )
 
 table_creation
