e29372ec94f897fd2ef3dc5bd77202a51eb66208
Cory Platt <corplatt@publicisgroupe.net>
2023-02-24 14:14:01 -0800

    Merge branch 'main' of https://github.com/eanrollings/airflow-dag-sephora-dev

--- hello_world.py
+++ hello_world.py
@@ -12,7 +12,7 @@ default_dag_args = {
     # fixed point in time rather than dynamically, since it is evaluated every
     # time a DAG is parsed. See:
     # https://airflow.apache.org/faq.html#what-s-the-deal-with-start-date
-    'start_date': datetime.datetime(2018, 1, 1),
+    "retries": 2,
 }
 
 # Define a DAG (directed acyclic graph) of tasks.
@@ -20,7 +20,9 @@ default_dag_args = {
 # DAG object.
 with models.DAG(
         'composer_sample_simple_greeting',
-        schedule_interval=datetime.timedelta(days=1),
+        description="DAG attempt at something",
+        schedule_interval="@daily",
+        start_date=datetime.datetime(2023, 1, 1),
         default_args=default_dag_args) as dag:
     def greeting():
         import logging
