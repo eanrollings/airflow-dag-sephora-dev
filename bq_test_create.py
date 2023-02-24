a49acd990e72553eee8fe0dc91b9874ed0b62eca
Cory Platt <corplatt@publicisgroupe.net>
2023-02-24 14:39:42 -0800

    making an update

--- bq_test_create.py
+++ bq_test_create.py
@@ -1,5 +1,21 @@
+from airflow import models
 from airflow.providers.google.cloud.operators.bigquery import BigQueryExecuteQueryOperator
+import datetime
 
+default_dag_args = {
+    # The start_date describes when a DAG is valid / can be run. Set this to a
+    # fixed point in time rather than dynamically, since it is evaluated every
+    # time a DAG is parsed. See:
+    # https://airflow.apache.org/faq.html#what-s-the-deal-with-start-date
+    "retries": 2,
+}
+
+with models.DAG(
+        'big query table creation',
+        description="DAG attempt at something",
+        schedule_interval="@daily",
+        start_date=datetime.datetime(2023, 1, 1),
+        default_args=default_dag_args) as dag:
 
 table_creation = BigQueryExecuteQueryOperator(
     task_id="make a table in bigquery",
